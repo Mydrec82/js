1276d9548c6dee1712945973fb29b94c11e0cf9f
Serega <mudrec82@gmail.com>
2025-10-02 21:01:05 +0300

    add movie dataDB

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const work = document.querySelector(".work");
-work.addEventListener("click", ()=> {
-    
-})
+const nuberOfFilms = prompt("сколько фильммов вы посмотрели ?", "");
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    private : false,
+}
+
+const a = prompt("последний просмотренный фильм ?", ""),
+        b = prompt("его оценка?", ""),
+        c = prompt ("последний просмотренный фильм ?",""), 
+        d = prompt("его оценка?", "");
