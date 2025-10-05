0b815e9232e01cec6bec60005a7a02d645fe26f8
Serega <mudrec82@gmail.com>
2025-10-05 20:44:35 +0300

    cycles

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const nuberOfFilms = prompt("сколько фильммов вы посмотрели ?", "");
+// const numberOfFilms = prompt("сколько фильммов вы посмотрели ?", "");
 
-const personalMovieDB = {
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    private : false,
-}
+// const personalMovieDB = {
+//     count : numberOfFilms,
+//     movies : {},
+//     actors : {},
+//     genres : [],
+//     private : false,
+// }
 
-const a = prompt("последний просмотренный фильм ?", ""),
-        b = prompt("его оценка?", ""),
-        c = prompt ("последний просмотренный фильм ?",""), 
-        d = prompt("его оценка?", "");
+// const a = prompt("последний просмотренный фильм ?", ""),
+//         b = prompt("его оценка?", ""),
+//         c = prompt ("последний просмотренный фильм ?",""), 
+//         d = prompt("его оценка?", "");
+
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB.movies)
+
+console.log(Boolean(undefined))
